f1278a1b7c2066806709f8d86795897de4f3d045
psysa14 <psysa14@nottingham.ac.uk>
2023-12-15 16:03:50 +0000

    Added glass

--- raytracing/main.cpp
+++ raytracing/main.cpp
@@ -12,13 +12,14 @@ int main() {
     hittableList world;
 
     auto materialGround = make_shared<lambertian>(colour(0.8, 0.8, 0.0));
-    auto materialCenter = make_shared<lambertian>(colour(0.7, 0.3, 0.3));
-    auto materialLeft = make_shared<metal>(colour(0.8, 0.8, 0.8), 0.3);
-    auto materialRight = make_shared<metal>(colour(0.8, 0.6, 0.2), 1.0);
+    auto materialCenter = make_shared<lambertian>(colour(0.1, 0.2, 0.5));
+    auto materialLeft = make_shared<dielectric>(1.5);
+    auto materialRight = make_shared<metal>(colour(0.8, 0.6, 0.2), 0.7);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, materialGround));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, materialCenter));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, materialLeft));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, materialLeft));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, materialRight));
 
     camera cam;
